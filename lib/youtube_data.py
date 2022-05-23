7e25bfd9ba7b6a2acdde198391716e269eb3c7c7
Fredpwol <fredpwol@gmail.com>
2022-05-23 22:00:35 +0100

    initial commit

---
+++ lib/youtube_data.py
@@ -0,0 +1,16 @@
+
+
+class YouTubeData(object):
+
+    def __init__(self) -> None:
+        pass
+
+    def list_all_subtitles(self) -> None:
+        pass
+
+    def get_subtitle(self, lang):
+        pass
+
+    def download_audio_track(self):
+        pass
+
