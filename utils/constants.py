a24ebd823f23fb8090f4adfb95f0f5dab48d6f92
Fredpwol <fredpwol@gmail.com>
2022-06-01 22:07:36 +0100

    update regexes

--- utils/constants.py
+++ utils/constants.py
@@ -3,7 +3,9 @@ ACCEPTABLE_INLINE_TAGS = ['break']
 
 ENCLOSED_TAG_PATTERN = r'\<([{0}]+)\s*([^>]*)\>([\w| \s| \d | \W]*)\<\/([{0}]+)\>.*'.format(' | '.join(ACCEPTABLE_TAGS))
 
-TEXT_PATTERN = r'\s*([\w | \s | . | ,]*)(?:<[\w|\s|-]+.*>)'
+TEXT_PATTERN = r'\s*([\w | \s | . | ,]*)(?:<[\w|\s|-]+.*>)*'
 
-INLINE_TAG_PATTERN = r'(\<[{0}]\s*[^/>]*/>)'.format('|'.join(ACCEPTABLE_INLINE_TAGS))
+INLINE_TAG_PATTERN = r'(\<([{0}]+)\s*([^/>]*)/>)'.format('|'.join(ACCEPTABLE_INLINE_TAGS))
 
+TAG_PATTERN =  r'\<([{0}]+)\s*([^/>]*)>'.format('|'.join(ACCEPTABLE_TAGS))
+CLOSE_TAG_PATTERN =  r'\<\/([{0}]+)\s*>'.format('|'.join(ACCEPTABLE_TAGS))
